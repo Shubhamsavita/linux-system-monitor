647f8a92d0558d9c6bab742eb2956bfa0748be47
Shubham Savita <shubhamsavita008@gmail.com>
2026-06-21 20:00:34 +0530

    Added memory dish and uptime monitoring

--- monitor.py
+++ monitor.py
@@ -1,10 +1,23 @@
+import os
 import platform
 import socket
 import getpass
 
 print("====Linux System Monitor====")
+
+
 print("Hostname:",socket.gethostname())
 print("User:",getpass.getuser())
 print("Operating System:", platform.system())
 print("OS Version:", platform.release())
 print("Python Version:",platform.python_version())
+
+print("\n====Memory====")
+os.system("free -h")
+
+print("\n====Disk====")
+os.system("df -h /")
+
+print("\n====Uptime====")
+os.system("uptime")
+
